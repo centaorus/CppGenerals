7867b552116339335e5814ea735ed7bc557e6b14
Carlos Pereira <c.pereira@pimenta.group>
2022-09-27 20:35:19 -0300

    Simple C++ (C plus plus) multi-line output program

---
+++ OUTPUT_02_SimpleMultiline.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "|x" << endl;
+    cout << "| x" << endl;
+    cout << "|  x" << endl;
+    cout << "|   x" << endl;
+    cout << "|    x" << endl;
+    cout << "|     x" << endl;
+    cout << "|      x" << endl;
+    cout << "|       x" << endl;
+    cout << "|        x" << endl;
+    cout << "|         x" << endl;
+    cout << "|          x" << endl;
+    cout << "|           x" << endl;
+    cout << "|____________x" << endl;
+    return 0;
+}
